3ec1e01418ec1cddd8fafa4cd65ebc82147e4094
ant31 <2t.antoine@gmail.com>
2016-03-05 21:00:54 +0100

    Ruby 2.3.0

--- Dockerfile.tpl
+++ Dockerfile.tpl
@@ -1,4 +1,3 @@
 FROM ruby:{{RUBY_VERSION}}
 
 RUN apt-get update && apt-get install curl python-pip git ansible openssh-client libcurl4-openssl-dev emacs-nox vim -y --no-install-recommends
-RUN pip install ansible --upgrade
--- update.sh
+++ update.sh
@@ -9,14 +9,11 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-travisEnv=
 for version in "${versions[@]}"; do
-    travisEnv='\n  - VERSION='"$version$travisEnv"
-    mkdir "$version" || true
     (
 	set -x
 	cp Dockerfile.tpl "build/Dockerfile"
 	sed -i 's/{{RUBY_VERSION}}/'"$version"'/g;' "build/Dockerfile"
     )
-    git tag $version -f
+    git commit -a -m "Ruby $version" || git tag $version -f
 done
